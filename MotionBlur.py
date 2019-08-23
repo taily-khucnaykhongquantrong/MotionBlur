1672d0210f63c8f928949eed2c9a969703eab741
young666 <lythanhtaiyoung666@gmail.com>
2019-08-23 16:28:55 +0700

    fix linekernel using nxn size

--- MotionBlur.py
+++ MotionBlur.py
@@ -7,18 +7,15 @@ from skimage.draw import line
 
 from LineDictionary import LineDictionary
 
-lineLengths = [3, 5, 7, 9]
 lineTypes = ["full", "right", "left"]
-
-lineDict = LineDictionary()
 
 
 def LinearMotionBlur_random(img):
-    lineLengthIdx = np.random.randint(0, len(lineLengths))
     lineTypeIdx = np.random.randint(0, len(lineTypes))
-    lineLength = lineLengths[lineLengthIdx]
+    lineLength = np.random.randint(0, 20)
     lineType = lineTypes[lineTypeIdx]
     lineAngle = randomAngle(lineLength)
+
     return LinearMotionBlur(img, lineLength, lineAngle, lineType)
 
 
@@ -65,13 +62,16 @@ def LineKernel(dim, angle, linetype="full"):
     kernelCenter = int(math.floor(dim / 2))
     angle = SanitizeAngleValue(kernelCenter, angle)
     kernel = np.zeros((kernelwidth, kernelwidth), dtype=np.float32)
+    lineDict = LineDictionary(kernelwidth)
     lineAnchors = lineDict.lines[dim][angle]
+
     if linetype == "right":
         lineAnchors[0] = kernelCenter
         lineAnchors[1] = kernelCenter
     if linetype == "left":
         lineAnchors[2] = kernelCenter
         lineAnchors[3] = kernelCenter
+
     rr, cc = line(lineAnchors[0], lineAnchors[1], lineAnchors[2], lineAnchors[3])
     kernel[rr, cc] = 1
     normalizationFactor = np.count_nonzero(kernel)
